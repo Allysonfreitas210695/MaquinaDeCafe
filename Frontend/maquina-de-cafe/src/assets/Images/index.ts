3cfb99083e4606116173acde3f0280a77c7257a2
pedrodamiaoluz <pedrodamiao26061998@gmail.com>
2025-04-27 15:22:10 -0300

    feat:Adicionei as imagens do projeto e fiz um index.ts para quando for chamar ela.

---
+++ Frontend/maquina-de-cafe/src/assets/Images/index.ts
@@ -0,0 +1,13 @@
+import CafeExpresso from "./CafeExpresso.png";
+import CafeAmericano from "./CafeAmericano.png";
+import Cappuccino from "./Cappuccino.png";
+import CafeComLeite from "./CafeComLeite.png";
+import Mocaccino from "./Mocaccino.png";
+
+export const Images = {
+  CafeExpresso,
+  CafeAmericano,
+  Cappuccino,
+  CafeComLeite,
+  Mocaccino,
+};
