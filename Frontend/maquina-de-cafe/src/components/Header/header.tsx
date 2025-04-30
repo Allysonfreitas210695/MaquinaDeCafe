ace2315d7512d684a8234c774581f676a9778f3e
pedrodamiaoluz <pedrodamiao26061998@gmail.com>
2025-04-30 09:00:52 -0300

    perf:fiz uma alteração no codigo para pelhorar o desempenho no componente header

--- Frontend/maquina-de-cafe/src/components/Header/header.tsx
+++ Frontend/maquina-de-cafe/src/components/Header/header.tsx
@@ -1,20 +1,32 @@
-
+import { IconType } from "react-icons";
 import { Images } from "../../assets/Images";
-import * as S from "./style"
+import * as S from "./style";
 import { IoCartSharp } from "react-icons/io5";
+import React from "react";
 
+interface IHeaderProps {
+  Localizacao: string;
+  Icon: IconType;
+}
 
+const headerProps: IHeaderProps[] = [
+  { Localizacao: "Pau dos Ferros, RN", Icon: IoCartSharp },
+];
 
 export const Header = () => {
-    return (
-        <S.Container__Header>
-            <S.Logo__Header src={Images.Logo}/>
-            <S.Conteudo__Header>
-                <S.Localizacao>
-                    <S.Span>Pau dos Ferros, RN</S.Span>
-                </S.Localizacao>
-                <S.Carrinho><IoCartSharp /></S.Carrinho>
-            </S.Conteudo__Header>
-        </S.Container__Header>
-    )
-}
+  return (
+    <S.Container__Header>
+      {headerProps.map(({ Localizacao, Icon }) => (
+        <React.Fragment>
+          <S.Logo__Header src={Images.Logo} />
+          <S.Conteudo__Header>
+            <S.Localizacao>
+              <S.Span>{Localizacao}</S.Span>
+            </S.Localizacao>
+            <S.Carrinho>{<Icon />}</S.Carrinho>
+          </S.Conteudo__Header>
+        </React.Fragment>
+      ))}
+    </S.Container__Header>
+  );
+};
