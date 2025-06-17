7ab8a48f1e31b971050353fbc4a160fca93042f1
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-06-17 19:26:50 -0300

    chore: adicionando notação para tornar obrigatório o campo

--- Backend/MaquinaDeCafe/src/Communication/Request/RequestAtualizacaoCafeJson.cs
+++ Backend/MaquinaDeCafe/src/Communication/Request/RequestAtualizacaoCafeJson.cs
@@ -6,6 +6,7 @@ public class RequestAtualizacaoCafeJson
 {
     public string Nome { get; set; } = string.Empty;
     public string Descricao { get; set; } = string.Empty;
+    [Required(ErrorMessage = "O campo 'Preco' é obrigatório.")]
     public decimal Preco { get; set; }
     public int TempoPreparoSegundos { get; set; }
     public CategoriaCafe Categoria { get; set; }
