3260d837dd5de2ee6262c162f0259a9b8f87538b
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-06-17 19:39:55 -0300

    chore: adicionando correção conforme sonar mostrou

--- Backend/MaquinaDeCafe/src/Communication/Request/RequestCriacaoIngredienteAdicionalJson.cs
+++ Backend/MaquinaDeCafe/src/Communication/Request/RequestCriacaoIngredienteAdicionalJson.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 namespace MaquinaDeCafe.src.Communication.Request;
 
 public class RequestCriacaoIngredienteAdicionalJson
 {
     public string Nome { get; set; } = string.Empty;
+    [Required(ErrorMessage = "O campo 'ValorExtra' é obrigatório.")]
     public decimal ValorExtra { get; set; }
 }
--- Backend/MaquinaDeCafe/src/Communication/Request/RequestTamanhoXicaraJson.cs
+++ Backend/MaquinaDeCafe/src/Communication/Request/RequestTamanhoXicaraJson.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MaquinaDeCafe.src.Communication.Request;
 
 public class RequestTamanhoXicaraJson
 {
     public string Descricao { get; set; } = string.Empty;
     public int Ml { get; set; }
+    [Required(ErrorMessage = "O campo 'ValorExtra' é obrigatório.")]
     public decimal ValorExtra { get; set; }
 }
