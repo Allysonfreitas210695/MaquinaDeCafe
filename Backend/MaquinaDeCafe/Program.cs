c47cc331df292310f996145d00d307b79403f8cc
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-07-19 23:35:54 -0300

    chore: Adicioanando dados no novo banco azure

--- Backend/MaquinaDeCafe/Program.cs
+++ Backend/MaquinaDeCafe/Program.cs
@@ -8,18 +8,12 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// -------------------- Configuração de Serviços --------------------
-
 builder.Services
     .AddControllers()
     .AddNewtonsoftJson(options =>
     {
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-    })
-    .AddJsonOptions(opt =>
-    {
-        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
@@ -30,70 +24,65 @@ builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy", cors =>
-    {
-        cors.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .WithExposedHeaders("X-Pagination", "X-TotalCount", "X-TotalPages", "X-PageSize", "X-CurrentPage");
-    });
+    options.AddPolicy("CorsPolicy",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader()
+                   .WithExposedHeaders("X-Pagination", "X-TotalCount", "X-TotalPages", "X-PageSize", "X-CurrentPage");
+        });
 });
 
-// -------------------- Banco de Dados --------------------
+builder.Services.AddControllers()
+.AddJsonOptions(opt =>
+{
+    opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// -------------------- Injeção de Dependência --------------------
-
-builder.Services.AddScoped<IAvaliacaoCafeRepository, AvaliacaoCafeService>();
-builder.Services.AddScoped<ICafeRepository, CafeService>();
-builder.Services.AddScoped<IIngredienteAdicionalRepository, IngredienteAdicionalService>();
-builder.Services.AddScoped<IPedidoRepository, PedidoService>();
-builder.Services.AddScoped<ITamanhoXicaraRepository, TamanhoXicaraService>();
-
-// -------------------- Swagger --------------------
-
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// -------------------- Ajuste para Azure App Service --------------------
-
-builder.WebHost.UseUrls("http://+:80");
-
-// -------------------- Pipeline HTTP --------------------
+builder.Services.AddScoped<IAvaliacaoCafeRepository, AvaliacaoCafeService>();
+builder.Services.AddScoped<ICafeRepository, CafeService>();
+builder.Services.AddScoped<IIngredienteAdicionalRepository, IngredienteAdicionalService>(); builder.Services.AddScoped<IPedidoRepository, PedidoService>();
+builder.Services.AddScoped<IPedidoRepository, PedidoService>();
+builder.Services.AddScoped<ITamanhoXicaraRepository, TamanhoXicaraService>();
 
 var app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
-
-app.UseRouting();
 
 app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
-// Rota de teste para verificar se o app está vivo
-app.MapGet("/", () => "☕ API Máquina de Café no ar!");
+await MigrateDatabase();
 
-// -------------------- Opcional: Migrations/Seed --------------------
-// await MigrateDatabase();
-// await SeedDatabase();
+await SeedDatabase();
 
 app.Run();
 
+async Task MigrateDatabase()
+{
+    await using var scope = app.Services.CreateAsyncScope();
 
-// async Task MigrateDatabase()
-// {
-//     await using var scope = app.Services.CreateAsyncScope();
-//     await DataBaseMigration.MigrateDatabase(scope.ServiceProvider);
-// }
+    await DataBaseMigration.MigrateDatabase(scope.ServiceProvider);
+}
 
-// async Task SeedDatabase()
-// {
-//     await using var scope = app.Services.CreateAsyncScope();
-//     await SeedDatabaseInitial.Seeds(scope.ServiceProvider);
-// }
+async Task SeedDatabase()
+{
+    await using var scope = app.Services.CreateAsyncScope();
+
+    await SeedDatabaseInitial.Seeds(scope.ServiceProvider);
+}
