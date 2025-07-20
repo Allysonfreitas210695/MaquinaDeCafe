47372a3a07e081dead9da859071fdc808faa81e1
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-07-19 23:03:11 -0300

    chore: Testando erro de Azure

--- Backend/MaquinaDeCafe/Program.cs
+++ Backend/MaquinaDeCafe/Program.cs
@@ -8,12 +8,18 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// -------------------- Configuração de Serviços --------------------
+
 builder.Services
     .AddControllers()
     .AddNewtonsoftJson(options =>
     {
         options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         options.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+    })
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     });
 
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
@@ -24,34 +30,38 @@ builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy",
-        builder =>
-        {
-            builder.AllowAnyOrigin()
-                   .AllowAnyMethod()
-                   .AllowAnyHeader()
-                   .WithExposedHeaders("X-Pagination", "X-TotalCount", "X-TotalPages", "X-PageSize", "X-CurrentPage");
-        });
+    options.AddPolicy("CorsPolicy", cors =>
+    {
+        cors.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .WithExposedHeaders("X-Pagination", "X-TotalCount", "X-TotalPages", "X-PageSize", "X-CurrentPage");
+    });
 });
 
-builder.Services.AddControllers()
-.AddJsonOptions(opt =>
-{
-    opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-});
+// -------------------- Banco de Dados --------------------
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// -------------------- Injeção de Dependência --------------------
 
 builder.Services.AddScoped<IAvaliacaoCafeRepository, AvaliacaoCafeService>();
 builder.Services.AddScoped<ICafeRepository, CafeService>();
-builder.Services.AddScoped<IIngredienteAdicionalRepository, IngredienteAdicionalService>(); builder.Services.AddScoped<IPedidoRepository, PedidoService>();
+builder.Services.AddScoped<IIngredienteAdicionalRepository, IngredienteAdicionalService>();
 builder.Services.AddScoped<IPedidoRepository, PedidoService>();
 builder.Services.AddScoped<ITamanhoXicaraRepository, TamanhoXicaraService>();
+
+// -------------------- Swagger --------------------
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// -------------------- Ajuste para Azure App Service --------------------
+
+builder.WebHost.UseUrls("http://+:80");
+
+// -------------------- Pipeline HTTP --------------------
 
 var app = builder.Build();
 
@@ -66,22 +76,24 @@ app.UseCors("CorsPolicy");
 
 app.MapControllers();
 
-// await MigrateDatabase();
+// Rota de teste para verificar se o app está vivo
+app.MapGet("/", () => "☕ API Máquina de Café no ar!");
 
+// -------------------- Opcional: Migrations/Seed --------------------
+// await MigrateDatabase();
 // await SeedDatabase();
 
 app.Run();
 
+
 // async Task MigrateDatabase()
 // {
 //     await using var scope = app.Services.CreateAsyncScope();
-
 //     await DataBaseMigration.MigrateDatabase(scope.ServiceProvider);
 // }
 
 // async Task SeedDatabase()
 // {
 //     await using var scope = app.Services.CreateAsyncScope();
-
 //     await SeedDatabaseInitial.Seeds(scope.ServiceProvider);
 // }
