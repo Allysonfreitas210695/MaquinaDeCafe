1e17fa3f5dfc7880cbeb3f62d2dbdb98597be2af
liviabeatrizml <liviabeatrizmaia7@gmail.com>
2025-07-27 17:01:32 -0300

    fix: validação de pedido cancelado

--- Frontend/maquina-de-cafe/src/pages/Carrinho/carrinho.tsx
+++ Frontend/maquina-de-cafe/src/pages/Carrinho/carrinho.tsx
@@ -3,7 +3,6 @@ import * as S from "./style";
 import { useCart } from "../Carrinho/CardContext/cardcontext";
 import { CardPagamento } from "../Pagamento/CardPagamento/card";
 import { BsArrowLeftShort } from "react-icons/bs";
-import { Images } from "../../assets/Images";
 import Swal from 'sweetalert2';
 
 export const Carrinho = () => {
@@ -14,11 +13,19 @@ export const Carrinho = () => {
 
   const handleCancelar = () => {
     clearCart();
-    navigate("/pedido"); 
+    Swal.fire({
+      icon: "error",
+      title: "Pedido Cancelado!",
+      text: "Explore nosso devine café e comece um novo pedido!",
+      timer: 3000, 
+      showConfirmButton: false,
+    }).then(() => {
+      navigate("/pedido");
+    });
   };
 
   const handleContinuarComprando = () => {
-    navigate("/pedido"); 
+    navigate("/pedido");
   };
 
   const handleVoltar = () => {
@@ -43,15 +50,13 @@ export const Carrinho = () => {
     <S.Container__Carrinho>
       <S.AcoesTopo>
         <BsArrowLeftShort onClick={handleVoltar} className="short" />
-        <button onClick={handleCancelar}>Cancelar</button> 
+        <button onClick={handleCancelar}>Cancelar</button>
       </S.AcoesTopo>
 
       <S.Pedido__Escolha_Carrinho>
         {cart.length === 0 ? (
           <S.CarrinhoVazio>
             <p>Seu carrinho está vazio.</p>
-            <img src={Images.caffee} alt="Café Vazio" />
-            <button onClick={() => navigate('/')}>Voltar ao Início</button>
           </S.CarrinhoVazio>
         ) : (
           <div className="carrinho">
