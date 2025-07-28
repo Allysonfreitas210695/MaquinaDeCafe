ab207e4fead8ac315c2b42dc9dbc5a1839ed4f8a
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-07-28 13:33:02 -0300

    fix: Ajustando aviso do sonar

--- Backend/MaquinaDeCafe/src/Communication/Request/RequestCriacaoPedidoJson.cs
+++ Backend/MaquinaDeCafe/src/Communication/Request/RequestCriacaoPedidoJson.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using MaquinaDeCafe.src.Communication.Request;
 using MaquinaDeCafe.src.Models.Enums;
 
@@ -5,7 +6,9 @@ namespace MaquinaDeCafe.src.DTOs;
 
 public class RequestCriacaoPedidoJson
 {
-    public FormaPagamento FormaPagamento { get; set; }
+    [Required(ErrorMessage = "O campo 'FormaPagamento' é obrigatório.")]
+    public required FormaPagamento FormaPagamento { get; set; }
     public decimal ValorTotal { get; set; } = 0.0m;
     public List<RequestCafesPedidosJson> PedidosItens { get; set; } = default!;
 }
+ 
